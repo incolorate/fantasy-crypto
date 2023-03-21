3e28c20363debab1b23308821c4174eb887a80fd
incolorate <88613908+incolorate@users.noreply.github.com>
2023-03-21 13:39:51 +0200

    - buy multiple types of coins, store updates correctly

---
+++ src/components/Dashboard/Trade.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import Coin from "./Coin";
+
+function Trade() {
+  const { userName, wallet } = useSelector((state) => {
+    return state.user;
+  });
+
+  console.log(wallet);
+
+  const COINS = ["bitcoin", "ethereum", "binancecoin", "ripple", "cardano"];
+
+  let renderedCoins = COINS.map((coin) => {
+    return <Coin key={coin} coinType={coin} />;
+  });
+  return (
+    <div>
+      <div
+        className="bg-slate-300 bg-opacity-10 w-full p-8 
+                rounded-xl flex justify-between align-middle text-white mb-8"
+      >
+        <p className="text-2xl ">Welcome @{userName}</p>
+        <p className="text-2xl">Your wallet: ${wallet.USD}</p>
+      </div>
+      <div>{renderedCoins}</div>
+    </div>
+  );
+}
+
+export default Trade;
