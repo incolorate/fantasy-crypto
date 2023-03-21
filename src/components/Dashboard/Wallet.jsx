1af48182d2b4c289163ae761b8fd747728eac811
incolorate <88613908+incolorate@users.noreply.github.com>
2023-03-21 16:04:54 +0200

    -wallet displays all coins in state

--- src/components/Dashboard/Wallet.jsx
+++ src/components/Dashboard/Wallet.jsx
@@ -1,6 +1,40 @@
+import { useSelector } from "react-redux";
+
 function Wallet() {
-  //  TO DO
-  return <div></div>;
+  const { userName, wallet } = useSelector((state) => {
+    return state.user;
+  });
+
+  console.log(wallet);
+
+  return (
+    <div
+      className="text-white
+    "
+    >
+      <div
+        className="bg-slate-300 bg-opacity-10 w-full p-8 
+                rounded-xl flex justify-between align-middle mb-8"
+      >
+        <h1 className="text-3xl">Hello @{userName}</h1>
+      </div>
+      <div
+        className="bg-slate-300 bg-opacity-10 w-full p-8 
+                rounded-xl  "
+      >
+        {Object.keys(wallet).map((keyName) => (
+          <div
+            key={keyName}
+            className="bg-slate-300 bg-opacity-10 w-full p-8 
+          rounded-xl flex gap-6 mb-3 shadow-sm shadow-slate-200"
+          >
+            <p>{keyName}:</p>
+            <p>{wallet[keyName]}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Wallet;
